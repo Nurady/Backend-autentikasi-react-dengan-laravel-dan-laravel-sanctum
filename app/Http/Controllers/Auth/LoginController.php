d045d2cb68eb05ad0c2c6c82d7c1d2c3ae65dd58
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-12-12 20:27:16 +0800

    Endpoint-Login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class LoginController extends Controller
 {
@@ -15,6 +18,24 @@ class LoginController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        $user = User::whereEmail($request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ["The Credentials Incorrect"]
+            ]);
+        }
+
+        $user->tokens()->delete();
+        $token = $user->createToken('web-token');
+        return [
+            'user' => $user,
+            'token' => $token->plainTextToken,
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\{
+    RegisterController, LoginController
+};
 
-Route::post('register', RegisterController::class);
+Route::post('/register', RegisterController::class);
+Route::post('/login', LoginController::class);
 
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
