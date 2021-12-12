b8976fe9ee76a2c3f3172f8bf453467c308b968c
Nursyafriady Yadi <nursyafriady83@gmail.com>
2021-12-12 19:55:00 +0800

    Endpoint-Register

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class RegisterController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $data = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $data['password'] = bcrypt($request->password);
+        User::create($data);
+
+        return response()->json([
+            'message' => 'Success Registered'
+        ]);
+    }
+}
